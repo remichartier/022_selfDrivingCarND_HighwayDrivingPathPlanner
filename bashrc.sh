acc7c70594edb421287bd23859068634b401dfeb
remichartier <remipr.chartier@gmail.com>
2021-01-29 07:21:59 +0000

    add .gitignore bashrc.sh

---
+++ bashrc.sh
@@ -0,0 +1,5 @@
+git config --global user.email "remipr.chartier@gmail.com"
+git config --global user.name "remichartier"
+git remote set-url origin "git@github.com:remichartier/022_selfDrivingCarND_HighwayDrivingPathPlanner.git"
+eval "$(ssh-agent -s)"
+ssh-add .ssh/20201224_1219_github_ed25519
