8b1b88b310e98509e5264fea38b561093e6cb60b
remichartier <remipr.chartier@gmail.com>
2021-01-29 08:11:33 +0000

    comment test code to test simulator

--- src/main.cpp
+++ src/main.cpp
@@ -105,11 +105,15 @@ int main() {
            *   sequentially every .02 seconds
            */
           
+          /**
+          * Test code to go straight for first time testing simulator
+          
           double dist_inc = 0.5;
           for (int i = 0; i < 50; ++i) {
             next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
             next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
           }
+          */
 
 
           msgJson["next_x"] = next_x_vals;
