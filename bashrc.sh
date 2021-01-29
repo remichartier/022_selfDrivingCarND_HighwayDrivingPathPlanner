2c087623e7bba042a328d69ab1ce9df6726d6bd2
remichartier <remipr.chartier@gmail.com>
2021-01-29 08:08:34 +0000

    add apt install openssh-client to bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,5 +1,6 @@
 git config --global user.email "remipr.chartier@gmail.com"
 git config --global user.name "remichartier"
 git remote set-url origin "git@github.com:remichartier/022_selfDrivingCarND_HighwayDrivingPathPlanner.git"
+apt install openssh-client
 eval "$(ssh-agent -s)"
 ssh-add .ssh/20201224_1219_github_ed25519
